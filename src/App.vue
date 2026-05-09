1108eef90f13c3431879acd965e97b6f334218a7
egovaflavia <50259464+egovaflavia@users.noreply.github.com>
2026-05-10 00:17:28 +0700

    Refactor HelloWorld component to utilize props for dynamic greeting

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
+ /** 
+  * @Author: egovaflavia 
+  * @Date: 2026-05-10 00:10:47 
+  * @Desc: Props
+  * Props kependekan dari Properties, yaitu sebuah mekanisme untuk mengirim data dari komponen induk ke komponen anak. Props memungkinkan kita untuk membuat komponen yang lebih fleksibel dan dapat digunakan kembali dengan memberikan nilai yang berbeda pada setiap penggunaan komponen tersebut.  
+  */
 <template>
-  <HelloWorld />
+  <HelloWorld name="Egova"/>
+  <HelloWorld name="Riva"/>
+  <HelloWorld name="Gustino"/>
 </template>
