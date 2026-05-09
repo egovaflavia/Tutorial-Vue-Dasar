968973ba062cc073d3f9fe9d74a27e8df653bcd1
egovaflavia <50259464+egovaflavia@users.noreply.github.com>
2026-05-10 00:09:52 +0700

    refactor: simplify HelloWorld component and enhance App.vue structure

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,11 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <h1>Component Dasar</h1>
+  <HelloWorld />
+  <!-- Reusable -->
+  <HelloWorld />
+  <HelloWorld />
+  <HelloWorld />
   <HelloWorld />
 </template>
