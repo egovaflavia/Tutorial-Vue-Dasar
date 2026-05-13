98d18c664ebdd65187916ff23692b957f3cd9a57
egovaflavia <50259464+egovaflavia@users.noreply.github.com>
2026-05-14 00:12:08 +0700

    Add v-model component imports and update template to include them

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import VModelInput from './components/VModelInput.vue'
+import VModelTextArea from './components/VModelTextArea.vue'
+import VModelCheckbox from './components/VModelCheckbox.vue'
+import VModelSelect from './components/VModelSelect.vue'
+import VModelRadio from './components/VModelRadio.vue'
+
 </script>
 
 <template>
-  <HelloWorld />
+  <!-- <HelloWorld /> -->
+  <VModelInput />
+  <VModelTextArea />
+  <VModelCheckbox />
+  <VModelSelect />
+  <VModelRadio />
 </template>
